0a32de153281a387aae0b5731cab7b8271887466
janeny117 <jsja031017@kookmin.ac.kr>
2025-10-04 22:20:27 +0900

    feat: 회원가입 기능 및 DTO 추가, 사용자 서비스에 이메일 중복 검사 로직 구현

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UsersService } from 'src/users/users.service';
+import { User } from 'src/users/schemas/user.schema';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async registerUser(newUser: CreateUserDto): Promise<User> {
+    return await this.usersService.createUser(newUser);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schemas/user.schema';
+import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  async findByEmail(email: string): Promise<User | null> {
+    return await this.userModel.findOne({ email }).exec();
+  }
+
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const existingUser = await this.findByEmail(createUserDto.email);
+    if (existingUser) {
+      throw new ConflictException('이미 사용 중인 이메일 주소입니다.');
+    }
+    const salt = 10;
+    const hashedPassword = await bcrypt.hash(createUserDto.password, salt);
+    const createdUser = new this.userModel({
+      ...createUserDto,
+      password: hashedPassword,
+    });
+
+    return createdUser.save();
+  }
+}
