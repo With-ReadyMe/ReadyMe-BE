0983fe8e38d7c21658ec2dbefebfb989348e76b3
janeny117 <jsja031017@kookmin.ac.kr>
2025-10-04 23:07:31 +0900

    feat: 환경 변수 설정 및 사용자 등록 기능 구현

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -7,7 +8,20 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest'),
+    // 2. ConfigModule을 전역(Global)으로 설정하여 어디서든 환경 변수에 접근 가능하게 함
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    // 3. MongooseModule에 forRootAsync 적용
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('DATABASE_URL'),
+      }),
+      inject: [ConfigService], // ConfigService 주입
+    }),
+
     AuthModule,
     UsersModule,
   ],
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './schemas/user.schema';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('/register')
+  async create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.usersService.createUser(createUserDto);
+  }
+}
