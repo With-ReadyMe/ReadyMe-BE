906c895062c7eb52ac54c3f97acdcf9e9bbb5521
janeny117 <jsja031017@kookmin.ac.kr>
2025-10-05 21:13:14 +0900

    feat: 로그인 기능을 위한 DTO 및 사용자 인증 로직 추가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { UsersService } from 'src/users/users.service';
 import { User } from 'src/users/schemas/user.schema';
+import { LoginDto } from 'src/users/dto/login.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -9,5 +11,15 @@ export class AuthService {
 
   async registerUser(newUser: CreateUserDto): Promise<User> {
     return await this.usersService.createUser(newUser);
+  }
+  async validateUser(loginDto: LoginDto): Promise<any> {
+    const { email, password } = loginDto;
+    const user = await this.usersService.findByEmail(email);
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password: _, ...result } = user;
+      return result;
+    }
+    return null;
   }
 }
