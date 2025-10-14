c52ebe496b2477e8ad51986ef8607edba870e357
Eunji <hyni03@naver.com>
2025-10-14 16:26:20 +0900

    feat: JsonRespone 적용 및 모듈 수정 #4

--- src/application/auth/auth.module.ts
+++ src/application/auth/auth.module.ts
@@ -1,18 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthController } from './controller/auth.controller';
 import { AuthService } from './service/auth.service';
 import { DbModule } from 'src/application/DB/db.module';
-import { JwtModule } from '@nestjs/jwt';
+import { WinstonLoggerService } from 'src/core/interceptors/logging/winston-logger.service';
+import { JwtService } from '@nestjs/jwt';
+import { JwtUtil } from 'src/core/utils/jwt';
+import jwtConfig from 'src/config/jwt.config';
 
 @Module({
-  imports: [
-    DbModule,
-    JwtModule.register({
-      secret: 'SecretKey',
-      signOptions: { expiresIn: '60m' },
-    }),
-  ],
+  imports: [DbModule, ConfigModule.forFeature(jwtConfig)],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [WinstonLoggerService, JwtUtil, JwtService, AuthService],
 })
 export class AuthModule {}
--- src/application/auth/controller/auth.controller.ts
+++ src/application/auth/controller/auth.controller.ts
@@ -1,21 +1,32 @@
-import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Post, Body, UsePipes } from '@nestjs/common';
+import { CustomValidationPipe } from 'src/core/pipes/validation';
 import { AuthService } from '../service/auth.service';
-import { LoginDto } from 'src/application/auth/dto/login.dto';
-import { CreateUserDto } from 'src/application/auth/dto/create-user.dto';
-import { User } from 'src/application/DB/entity/user.entity';
+import { CreateUserDto, LoginDto } from 'src/application/auth/dto/auth.dto';
+import { JsonResponse } from 'src/core/utils/json-response';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/register')
-  async register(@Body() createUserDto: CreateUserDto): Promise<User> {
-    return this.authService.registerUser(createUserDto);
+  @UsePipes(CustomValidationPipe)
+  async register(@Body() createUserDto: CreateUserDto) {
+    const result = await this.authService.registerUser(createUserDto);
+
+    const response = new JsonResponse();
+    response.set('data', result);
+
+    return response.of();
   }
 
   @Post('/login')
-  @HttpCode(HttpStatus.OK)
+  @UsePipes(CustomValidationPipe)
   async login(@Body() loginDto: LoginDto) {
-    return this.authService.login(loginDto);
+    const result = await this.authService.login(loginDto);
+
+    const response = new JsonResponse();
+    response.set('data', result);
+
+    return response.of();
   }
 }
